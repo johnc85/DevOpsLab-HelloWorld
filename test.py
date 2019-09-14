88ba57e0343e20b50e514e9553ba474aaa39780f
johncaracho <johncaracho@yahoo.com.br>
2019-09-14 17:00:33 -0400

    update test.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Aula Lab 4linux")
+        self.assertRegex(result.data.decode(), "Lab 4Linux John")
 
 
 if __name__ == "__main__":
